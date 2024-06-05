28f1596722d0bb44abbea17327dcc3044a81862b
Ivan Chukayev <ichukayev@gmaill.com>
2024-06-05 13:40:13 +0500

    Added RatesStorageConcurrent version with ConcurrentDictionary

---
+++ ConsoleApp5/RatesStorageConcurrent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class RatesStorageConcurrent
+    {
+        private readonly ConcurrentDictionary<string, Rate> rates = new();
+
+       /// <summary>
+        /// этим методом мы копируем котировки из нативного класса в базовый класс нашей программы, 
+        /// обновляем котировки много и часто, будем считать, что у нас 1 млн вызовов этого метода в сутки
+        /// </summary>
+        /// <param name="newRate"></param>
+        public void UpdateRate(NativeRate newRate)
+        {
+            var newRateCopy = new Rate
+            {
+                Time = newRate.Time,
+                Symbol = newRate.Symbol,
+                Bid = newRate.Bid,
+                Ask = newRate.Ask
+            };
+
+            rates.AddOrUpdate(newRate.Symbol,
+                newRateCopy,
+                (key, existingRate) => newRateCopy);
+        }
+        /// <summary>
+        /// это тоже высоконагруженный метод, вызываем 1 млн раз в сутки
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
+        public Rate? GetRate(string symbol)
+        {
+            rates.TryGetValue(symbol, out var rate);
+
+            //if (rate.Ask < rate.Bid)
+            //{
+            //    throw new Exception($"Incorrect rate: Ask < Bid; Ask={rate.Ask}, Bid={rate.Bid}");
+            //}
+
+            return rate;
+        }
+
+    }
+}
---
+++ TestProject1/RatesStorageConcurrentTest.cs
@@ -0,0 +1,90 @@
+﻿using ConsoleApp5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class RatesStorageConcurrentTest
+    {
+        [Fact]
+        public void GetRate_Symbol_ShouldReturnSymbolRate()
+        {
+            //Arrange
+            var rateStorage = new RatesStorageConcurrent();
+            rateStorage
+                .UpdateRate(new NativeRate
+                {
+                    Symbol = "EURUSD",
+                    Bid = 1.42,
+                    Ask = 1.45,
+                    Time = DateTime.Now
+                });
+
+            //Act
+            var rate = rateStorage.GetRate("EURUSD");
+
+            //Assert
+            var expected = new Rate { Symbol = "EURUSD" }.Symbol;
+            var actual = rate?.Symbol;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetRate_SymbolNotExisted_ShouldReturnNull()
+        {
+            //Arrange
+            var rateStorage = new RatesStorageConcurrent();
+
+            //Act
+            var rate = rateStorage.GetRate("EURUSD");
+
+            //Assert
+            Assert.Null(rate);
+        }
+
+        [Fact]
+        public void GetRateUpdateRate_ConcurrentAccess_BidShouldLessOrEqualThenAsk()
+        {
+            var storage = new RatesStorageConcurrent();
+            var symbols = new[] { "EURUSD", "USDJPY", "GBPUSD" };
+
+            // Запускаем задачи для обновления и чтения котировок
+            var tasks = new List<Task>();
+
+            foreach (var symbol in symbols)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        storage.UpdateRate(new NativeRate
+                        {
+                            Time = DateTime.UtcNow,
+                            Symbol = symbol,
+                            Bid = i,
+                            Ask = i + 0.1
+                        });
+                    }
+                }));
+
+                tasks.Add(Task.Run(() =>
+                {
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        var rate = storage.GetRate(symbol);
+                        if (rate != null)
+                        {
+                            Assert.True(rate.Bid <= rate.Ask);
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
